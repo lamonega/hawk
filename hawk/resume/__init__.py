17bd3422d7efb6b5a8cc1f7174d3663b7dbbffd7
Laureano Lamonega <laureanolamonega@gmail.com>
2026-08-15 12:24:40 -0300

    chore: drop obsolete files superseded by unified rewrite

--- hawk/resume/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Resume rendering, tailoring, and export."""
-
-from hawk.resume.generator import generate_tailored_pdf, render_ats_resume_html
-
-__all__ = ["generate_tailored_pdf", "render_ats_resume_html"]
